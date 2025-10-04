72da2c7bf906d62306f019935fef618582eea4b8
sachin-nimbal <sachin.nimbal@in.pradeepit.com>
2025-10-04 10:17:25 +0530

    initial

---
+++ src/main/java/io/github/sachinnimbal/crudx/CrduxStarterApplication.java
@@ -0,0 +1,13 @@
+package io.github.sachinnimbal.crudx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrduxStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrduxStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/sachinnimbal/crudx/CrduxStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sachinnimbal.crudx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrduxStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
